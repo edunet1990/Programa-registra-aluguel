811355721fee186c8cc6c8bdb7020efd17840b18
Eduardo Estevão <eduardo.estevao.ext@pdg.com.br>
2018-09-24 15:14:11 -0300

    Criei um menu e dei funcionalidades para os ítens 1,2,3,4

--- RegistraQuartos/Program.cs
+++ RegistraQuartos/Program.cs
@@ -4,19 +4,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace RegistraQuartos
 {
     class Program
     {
         static void Main(string[] args)
         {
+            bool escolha = true;
             Registro R = new Registro();
-            Console.Write("Quantos aluguéis serão registrados? ");
-            R.N = int.Parse(Console.ReadLine());
 
+            // Console.Write("Quantos aluguéis serão registrados? ");
+            // R.N = int.Parse(Console.ReadLine());
 
-            R.Registra(R.N);
-            R.MostraRegistros(R.N);
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>> Bem vindo ao programa Registra Quarto <<<<<<<<<<<<<<<<<<");
+            Console.WriteLine("");
+
+            Console.WriteLine("Início");
+            Console.WriteLine("");
+            
+
+            //R.Escolha(R.N);
+
+            while (escolha)
+            {
+                Console.WriteLine("1 - Registrar um Quarto");
+                Console.WriteLine("2 - Quartos Disponíveis");
+                Console.WriteLine("3 - Quartos Reservados");
+                Console.WriteLine("4 - Reservas Registradas");
+                Console.WriteLine("5 - Excluir Reserva");
+                Console.WriteLine("6 - Sair");
+
+                R.N = int.Parse(Console.ReadLine());
+
+                switch (R.N)
+                {
+                    case 1:
+                        R.RegistraQuarto();
+                        
+                        break;
+
+                        case 2:
+                        R.QuartosDisponiveis();
+
+                        break;
+
+                    case 3:
+                        R.MostraRegistros();
+
+                        break;
+
+                    case 4:
+                        R.ReservasRegistradas();
+
+                        break;
+
+                    case 6:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+            
 
             Console.ReadKey();
 
--- RegistraQuartos/Registro.cs
+++ RegistraQuartos/Registro.cs
@@ -8,118 +8,172 @@ namespace RegistraQuartos
 {
     class Registro
     {
-        public int[] Quarto = new int[10];
+        public int[] Quarto = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         public string[] Nome = new string[10];
         public string[] Email = new string[10];
         public int[] QuartosEscolhidosArray = new int[10];
-        public int N,EscolhaDoQuarto,NumeroQuartosEscolhidos;
+        public int N,EscolhaDoQuarto,NumeroQuartosEscolhidos,ContaRegistros = 0,EnquantoRegistraQuarto = 1,ContaRegistrosQuartos = -1, ContaRegistrosDados = -1;
+        public string EscolheSeQuerRegistrarMais;
 
         
+        
+        public void RegistraQuarto()
+        {
+            while (EnquantoRegistraQuarto == 1)
+            {
+                ContaRegistrosQuartos++;
+                ContaRegistros++;
+                ContaRegistrosDados++;
+            //Mostra todos os quartos
 
-        public void Registra(int n) {
+            Console.Write("Quartos disponíveis: ");
+            for (int i = 0; i < Quarto.Length; i++)
+            {
+                Console.Write(Quarto[i]+"  ");
+            }
+
+            Console.WriteLine("");
+            Console.Write("Qual quarto deseja registrar?");
+            EscolhaDoQuarto = int.Parse(Console.ReadLine());
+
+                //guardando o quarto escolhido
+                QuartosEscolhidosArray[ContaRegistrosQuartos] = EscolhaDoQuarto;
+
+            //Procurando qual array é igual ao digitado para retirar da lista
+                bool escolha = false;
+            int es = EscolhaDoQuarto;
+            for (int e = 0; escolha == false; e++)
+            {
+                if (Quarto[e] == es)
+                {
+
+                    Quarto = Quarto.Where(item => item != es).ToArray();
+                    escolha = true;
+
+
+                }
+
+            }
+
+            Console.Write("Nome: ");
+            Nome[ContaRegistrosDados] = Console.ReadLine();
+
+            Console.Write("Email: ");
+            Email[ContaRegistrosDados] = Console.ReadLine();
+
+
+               
+            Console.WriteLine("Deseja registrar mais quartos? \nS (sim)\nN (Não)");
+                EscolheSeQuerRegistrarMais = Console.ReadLine();
+
+                if (EscolheSeQuerRegistrarMais != "s")
+                {
+                    EnquantoRegistraQuarto = 0;
+                }
+            }
+
+        }
+
+        //Verificando quantos quartos disponíveis
+        public void QuartosDisponiveis()
+        { 
+                Console.WriteLine("Quartos diponíveis:");
+
+                for (int i = 0; i < Quarto.Length; i++)
+                {
+                    Console.Write(Quarto[i] + "  ");
+                }
+            Console.WriteLine("");
+
+        }
+
+        public void Registra() {
 
             //Deixando Todos os quartos Disponíveis
-            for (int i = 1; i < 10; i++)
-            {
-                Quarto[i] = i;
-            }
+            
 
-
-            //Verificando quantos quartos disponíveis
-            void QuartosDisponiveis()
-            {
-
-                Console.WriteLine("Quartos diposníveis:");
-
-                for (int i = 1; i < Quarto.Length; i++)
-                {
-                    Console.Write(Quarto[i] +", ");
-                }
+            //Registrando todos os dados necessários e exibindo quantos quartos estão disponíveis para saber qual registrar     
                 
-            }
 
-            void RetirandoOQuartoDaLista()
-            {
-                //Procurando qual array é igual ao digitado para retirar da lista
-
-                bool escolha = false;
-                int es = EscolhaDoQuarto;
-                for (int e = 0; escolha == false; e++)
-                {
-                    if (Quarto[e] == es)
-                    {
-
-                        Quarto = Quarto.Where(item => item != es).ToArray();
-                        escolha = true;
-
-                        
-                    }
-
-                }
-            }
-
-
-
-
-            //Registrando todos os dados necessários e exibindo quantos quartos estão disponíveis para saber qual registrar
-            for (int i = 0; i< n; i++)
-            {
-                Console.Write("Nome: ");
-                Nome[i] = Console.ReadLine();
-
-                Console.Write("Email: ");
-                Email[i] = Console.ReadLine();
-
-
-                QuartosDisponiveis();
-                Console.Write("Qual Quarto? ");
-                EscolhaDoQuarto = int.Parse(Console.ReadLine());
-                QuartosEscolhidosArray[i] = EscolhaDoQuarto;
-                
-                RetirandoOQuartoDaLista();
+           
 
 
                 Console.WriteLine("");
-
-                NumeroQuartosEscolhidos++;
-
-            }
+            
 
 
+
+               
 
         }
 
 
 
 
-        public void MostraRegistros(int qtdQuartosRegistrados)
+        public void MostraRegistros()
         {
             int quartosEscolhidos = EscolhaDoQuarto;
+
+            Console.WriteLine("Quartos Reservados:");
+
+
+            for (int i = 0; i < ContaRegistros; i++){                
+
+                    Console.Write(QuartosEscolhidosArray[i] + "  ");
+                }
+                Console.WriteLine("");
+            }
+
+
+
+
+        
+        public void ReservasRegistradas()
+        {
             int iq = 1;
-            for (int i = 0; i < NumeroQuartosEscolhidos; i++)
+            for (int i = 0; i < ContaRegistros; i++)
             {
+
+
+
+                Console.WriteLine("Dados da " + iq + "° reserva");
+                Console.WriteLine("Quarto: " + this.QuartosEscolhidosArray[i]);
+                Console.WriteLine("Nome: " + this.Nome[i]);
+                Console.WriteLine("Email: " + this.Email[i]);
                 
-                
-                Console.WriteLine("Dados do " + iq + "° aluguel");
-                Console.WriteLine("Nome: "+this.Nome[i]);
-                Console.WriteLine("Email: "+this.Email[i]);
-                Console.WriteLine("Quarto: "+this.QuartosEscolhidosArray[i]);
+                Console.WriteLine("");
+
+                ++iq;
+            }
+        }
+
+
+        public void ExcluirReserva(int reservaASerExcluida)
+        {
+            int iq = 1;
+            for (int i = 0; i < ContaRegistros; i++)
+            {
+
+
+
+                Console.WriteLine("Dados da " + iq + "° reserva");
+                Console.WriteLine("Quarto: " + this.QuartosEscolhidosArray[i]);
+                Console.WriteLine("Nome: " + this.Nome[i]);
+                Console.WriteLine("Email: " + this.Email[i]);
+
                 Console.WriteLine("");
 
                 ++iq;
             }
 
-
-            Console.WriteLine("Quartos ocupados: ");
-
-
-            for (int i = 0; i < NumeroQuartosEscolhidos; i++)
+            /*if (reservaASerExcluida == )
             {
-                
-                Console.WriteLine(this.QuartosEscolhidosArray[i] + ": "+
-                    this.Nome[i]+", " +
-                    this.Email[i]);
+
             }
+            ContaRegistros -= 1;
+            this.QuartosEscolhidosArray[i]);
+            this.Nome[i]);
+            this.Email[i]);*/
         }
 
 
